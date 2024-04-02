cf13a97a677335fc6ce8a4a739ba39b277807a6b
DharshDharsh12 <162670289+DharshDharsh12@users.noreply.github.com>
2024-04-02 18:43:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let randomBox;
+let alpha = 0.5;
+
+function createTiles(n) {
+  let tiles = "";
+  for (let i = 0; i < n; i++) {
+    tiles += `<div class='box' id='b${i}'></div>`;
+  }
+  document.querySelector(".wrapper").innerHTML = tiles;
+}
+
+function setBackgroundColor() {
+  let red = Math.round(Math.random() * 255);
+  let green = Math.round(Math.random() * 255);
+  let blue = Math.round(Math.random() * 255);
+  let baseColor = `rgb(${red},${green},${blue})`;
+  let allBoxes = document.querySelectorAll(".box");
+  allBoxes.forEach(function(ele) {
+    ele.style.backgroundColor = baseColor;
+  })
+  randomBox = Math.floor(Math.random() * 16);
+  alpha += 0.01;
+  document.querySelector(`#b${randomBox}`).style.backgroundColor = `rgba(${red},${green},${blue},${alpha})`
+}
+
+document.querySelector('.wrapper').addEventListener("click", function(ev) {
+  let rc = ev.target.id.slice(1);
+  if (rc == randomBox) {
+    setBackgroundColor();
+  }
+});
+
+createTiles(16);
+setBackgroundColor();
